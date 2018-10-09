82c0b69af3fe739812e7394a63adc6c0f171f9e1
Xiurong Yu <xiurongy3506@hstat.org>
2018-10-09 18:30:23 +0000

    fix ruby basics failures

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,27 @@
-def division
-    
+def division(num1, num2)
+    num1/num2
 end
 
-def argue
+def argue(statement)
+    return statement
   
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
+    return "Nice"
     
 end
 
-def pizza_party
-    
+def pizza_party (pizza="cheese")
+    pizza
 end
 
-def whisper
+
+def whisper(statement)
+    statement.downcase
     
 end
